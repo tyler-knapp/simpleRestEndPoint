3bd03b38e6c858dac16226d9d44e86910e2d0c88
tyler <tylerknapp25@gmail.com>
2022-01-30 14:49:58 -0500

    Added some input fields. Need to reposition them and add my NavBar Header in

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import {Button, TextField} from '@material-ui/core';
+import Header from './component/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Button color="primary" variant="contained">PRESS ME</Button>
-      <TextField id="name" label="Name" variant="outlined" />
+        <Button color="primary" variant="contained">PRESS ME</Button>
+        <TextField id="name" label="Name" variant="outlined" />
+        <TextField id="username" label="Username" variant="outlined" />
+        <TextField id="password" label="Password" variant="outlined" />
+    
     </div>
   );
 }
